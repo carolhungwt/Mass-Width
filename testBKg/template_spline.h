161b7bd94efa5813f2495fa0c28b391687a8c6d7
Wai Ting Hung <wahung@lxplus.cern.ch>
2017-06-21 11:51:49 +0200

    adding dbkgkin to data_bkg

---
+++ testBKg/template_spline.h
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+#include <string>
+#include <algorithm>
+#include "TSystem.h"
+#include "TMath.h"
+#include "TCanvas.h"
+#include "TAxis.h"
+#include "TH1.h"
+#include "TGaxis.h"
+#include "TString.h"
+#include "TChain.h"
+#include "RooGlobalFunc.h"
+#include "RooRealVar.h"
+#include "RooDataSet.h"
+#include "RooGaussModel.h"
+#include "RooRealIntegral.h"
+#include "RooDecay.h"
+#include "RooBMixDecay.h"
+#include "RooCategory.h"
+#include "RooBinning.h"
+#include "RooPlot.h"
+#include "RooNumIntConfig.h"
+#include "RooWorkspace.h"
+#include <ZZMatrixElement/MELA/interface/Mela.h>
+#include <ZZMatrixElement/MELA/interface/ScalarPdfFactory_HVV.h>
+#include "NCSplinePdfFactory_1D.h"
+#include "NCSplinePdfFactory_2D.h"
+#include "NCSplinePdfFactory_3D.h"
+
+using namespace RooFit;
+using namespace std;
+
+string ch_list[] = {"2e2mu"};
+char *input_address[200];
+RooRealSumPdf sumpdf_temp;
+
+vector<TH2F*> hist_template(3);
+vector< vector<RooNCSplinePdf_3D_fast*> > spline_template(3, vector<RooNCSplinePdf_3D_fast*>(3));
+vector<RooProdPdf*> prod_pdfs;
+vector<RooRealSumPdf> sumpdfs;
+
+vector<RooArgList> coeff_list;
+vector<RooArgList> pdf_list;
+
+// Input and output filenames
+TString hist_input = "/afs/cern.ch/user/w/wahung/work/CMSSW_8_0_26_patch1/src/HiggsAnalysis/CombinedLimit/Mass-Width/testBKg/dbkgkin_mreco_WS_cond_hist_ggH_2e2mu_125_mreco.root";
+TString spline_input = "/afs/cern.ch/user/w/wahung/work/CMSSW_8_0_26_patch1/src/HiggsMassConstraint_new/HiggsMassConstraint/test/";
+
+// User-Defined functions
+vector<RooNCSplinePdf_3D_fast*> extract_splines(RooWorkspace* w, std::string channel);
+vector<TH2F*> extract_templates(TString input_file);
+vector<RooNCSplinePdf_3D_fast*> extract_splines(RooWorkspace* w, string channel);
+//RooArgList prod_pdf(vector<RooNCSplinePdf_3D_fast*> splines, vector<TH2F*> th2fs, TString ch);
+RooArgList prod_pdf(RooWorkspace* w, TString input_file, string ch, int i);
+RooArgList writeFormula(RooWorkspace* w, TString tag);
+RooArgList extract_coeff(RooWorkspace* w, string tag);
